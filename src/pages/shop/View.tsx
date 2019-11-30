3c2174f0fcec7f2f6608a066f5515f46d0d67661
Doyeong <jeekc0308@gmail.com>
2019-11-30 20:41:48 +0900

    change to hooks

--- src/pages/shop/View.tsx
+++ src/pages/shop/View.tsx
@@ -1,36 +1,25 @@
-import React, { useState, useEffect, Component } from "react";
-import { withRouter, RouteComponentProps, Router } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { withRouter, RouteComponentProps } from "react-router-dom";
 import ShopProduct from "../../components/Page/Shop/ShopProduct";
 import { getOnlineStoreProductData } from "../../lib/api/getOnlineStoreData";
 
-class ShopViewPage extends Component {
-  constructor(props: any) {
-    super(props);
-  }
-  state = {
-    name: "",
-    data: { img: "", title: "", cost: 0, content: "" }
-  };
-  async componentDidMount() {
-    const {
-      match: { params }
-    } = this.props as any;
-    const data = await getOnlineStoreProductData(params.id);
-    console.log(data.data);
-    this.setState({ name: params.id, data: data.data });
-  }
+function ShopViewPage({ match: { params } }: RouteComponentProps) {
+  const [data, setData] = useState<any>(null);
+  useEffect(() => {
+    const { id } = params as any;
+    getOnlineStoreProductData(id).then(({ data }) => {
+      setData({ name: id, ...data });
+    });
+  }, []);
 
-  render() {
-    return (
-      <div>
-        <ShopProduct
-          title={this.state.data.title}
-          price={this.state.data.cost}
-          image={this.state.data.img}
-        />
-      </div>
-    );
+  if (!data) {
+    return <div>로딩 중...</div>;
   }
+  return (
+    <div>
+      <ShopProduct title={data.title} price={data.cost} image={data.img} />
+    </div>
+  );
 }
 
-export default ShopViewPage;
+export default withRouter(ShopViewPage);
