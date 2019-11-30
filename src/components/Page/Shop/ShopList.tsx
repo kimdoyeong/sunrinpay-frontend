831735eb3f02c8411d2fa9ae1835e1b9170c8f5c
이호준 <46421618+HADMARINE@users.noreply.github.com>
2019-11-30 11:14:01 +0900

    Update ShopList.tsx
    dafdajlsafds

--- src/components/Page/Shop/ShopList.tsx
+++ src/components/Page/Shop/ShopList.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components';
 import ShopItem from './ShopItem';
 import getOnlineStoreData from '../../../lib/api/getOnlineStoreData'
+import { Link } from '@storybook/router';
 
 const List = styled.div`
     display: flex;
@@ -14,26 +15,38 @@ const List = styled.div`
     }
 `;
 
+const ListWrapper = styled.div`
+    text-decoration: none;
+    color: black;
+`
+
 class Shoplist extends React.Component{
     state = {
         shopData : []
     }
     
-    async componentDidMount(){
+    async componentWillMount(){
         const onlineStoreData = await getOnlineStoreData();
         this.setState({
             shopData : onlineStoreData.data
         })
 
-        console.log(this.state.shopData)
+
     }
 
 
     render(){
         return (
+
             <List>
-                {this.state.shopData.map((data:any)=>{
-                    return <ShopItem itemName={data.title} price={data.cost} image={data.img}/>
+                {this.state.shopData.map((data:any,idx : number)=>{
+                    return(
+                        <ListWrapper>
+                            <a href={"/shop/" + data.title} style={{color:'black'}}>
+                                <ShopItem itemName={data.title} price={data.cost} image={data.img}/>
+                            </a>
+                        </ListWrapper>
+                    )
                 })}
             </List>
         )
